4c5429934e8808fafd5df70a89d5a8fc00277f4c
nvuono <nvuono@outlook.com>
2020-04-30 18:13:52 -0400

    login/logout & facility search sanity test
    add verify/login/logout api load test, added facility search api load test, cleaned up repo, added ignore for the config.json

--- src/pages/scripts/landing-batch.js
+++ src/pages/scripts/landing-batch.js
@@ -1,11 +1,12 @@
 import {group} from "k6";
 import batch from "../modules/batch.js";
 
-const landingPage = JSON.parse(open("../json/landing.json"));
+const landingPage = JSON.parse(open("../json/landing2.json"));
 
 export let options = { 
     vus: 100,
-    duration: "60s"
+    //duration: "60s",
+    iterations:600
 };
 
 export default function() {
